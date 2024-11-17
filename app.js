51fa6027767221791625b582393e792eb4c00871
Julio Syah Putra <juliosp2107@gmail.com>
2024-11-17 18:40:36 +0700

    feat: tokenVerifier, procRoutes and initializations

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-const express = require("express");
+const express = require('express');
+const admin = require('firebase-admin');
+const protectedRoutes = require('./routes/protectedRoutes');
 
 const app = express();
+const port = 3000;
 
-port = 3000;
+const serviceAccount = require('./config/serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://be-rasadhana.firebaseio.com',
+});
+
+app.use((req, res, next) => {
+  console.log(`Request received: ${req.method} ${req.url}`);
+  next();
+});
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my Express.js server!');
+});
+
+app.use('/api', protectedRoutes);
+
 app.listen(port, () => {
-  console.log(`Successfully connected to ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
