b99ad3f906e8145542242bc08b8b78f5a47f6bc2
Julio Syah Putra <juliosp2107@gmail.com>
2024-11-22 18:05:16 +0700

    feat: fully migrated to mongo, regis, login, forgot pass and encryption

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { UserRouter } from './routes/user.js';
+
+import mongoose from 'mongoose';
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+const mongoUrl = process.env.MONGO_URL;
+app.use(express.json());
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    console.log('Database Connected');
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.use('/auth', UserRouter);
+
+app.listen(PORT, () => {
+  console.log(`server running in port ${PORT}`);
+});
